4ecc1d74b19e4131c33892c600ba767086ef0b39
rlmelgar <rlmelgar@gmail.com>
2023-11-21 09:02:59 +0100

    [RLM-TR-BDD-7] feat: add get transmissions bdd test

--- bdd-test/src/test/java/cucumber/stepdefs/GetTransmissions.java
+++ bdd-test/src/test/java/cucumber/stepdefs/GetTransmissions.java
@@ -1,11 +1,18 @@
 package cucumber.stepdefs;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+import com.rlm.training.bdd.infrastructure.mongodb.document.TransmissionDocument;
 import com.rlm.training.bdd.infrastructure.mongodb.document.TransmissionDocumentBuilder;
 import com.rlm.training.bdd.infrastructure.mongodb.repository.TransmissionRepository;
 import com.rlm.training.bdd.infrastructure.rest.server.controller.TransmissionController;
 import com.rlm.training.bdd.infrastructure.rest.server.dto.TransmissionResponse;
-import com.rlm.training.bdd.infrastructure.rest.server.dto.TransmissionResponseBuilder;
 import io.cucumber.java.Before;
+import io.cucumber.java.DataTableType;
+import io.cucumber.java.ParameterType;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
@@ -34,10 +41,59 @@ public class GetTransmissions extends CucumberSpringConfiguration {
     webTestClient = MockMvcWebTestClient.bindToController(transmissionController).build();
   }
 
-  @Given("an active transmission stored with petitions")
-  public void an_active_transmission_stored_with_petitions() {
+  @ParameterType("active|with|inactive|without")
+  public Boolean booleanValue(String value) {
+    if ("active".equals(value) || "with".equals(value)) {
+      return Boolean.TRUE;
+    }
+    return Boolean.FALSE;
+  }
+
+  @ParameterType("a|an|one|two|tree")
+  public int numeric(String value) {
+    return switch (value) {
+      case "a", "an", "one" -> 1;
+      case "two" -> 2;
+      case "tree" -> 3;
+      default -> Integer.parseInt(value);
+    };
+  }
+
+  @Given("no transmission stored")
+  public void no_transmission_stored() {
     transmissionRepository.deleteAll();
-    transmissionRepository.save(TransmissionDocumentBuilder.build());
+  }
+
+  @Given("{numeric} {booleanValue} transmission(s) stored {booleanValue} petitions")
+  public void given_an_transmission_stored_petitions(Integer nDocuments, Boolean active, Boolean hasPetitions) {
+    List<TransmissionDocument> transmissionDocumentList = new ArrayList<>();
+    for (int i = 0; i < nDocuments; i++) {
+      TransmissionDocument transmissionDocument = TransmissionDocumentBuilder.build().setId(null).setActive(active);
+      transmissionDocument.getSpaceship().setName(transmissionDocument.getSpaceship().getName() + UUID.randomUUID());
+      if (!hasPetitions) {
+        transmissionDocument.setPetitions(List.of());
+      }
+      transmissionDocumentList.add(transmissionDocument);
+    }
+    transmissionRepository.saveAll(transmissionDocumentList);
+  }
+
+  @DataTableType
+  public TransmissionDocument transmissionDocumentTransformer(Map<String, String> row) {
+    TransmissionDocument transmissionDocument = TransmissionDocumentBuilder.build()
+        .setId(row.get("id"))
+        .setActive(Boolean.parseBoolean(row.get("active")));
+    transmissionDocument.getSpaceship().setName(transmissionDocument.getSpaceship().getName() + transmissionDocument.getId());
+
+    if (!Boolean.parseBoolean(row.get("hasPetitions"))) {
+      transmissionDocument.setPetitions(List.of());
+    }
+    return transmissionDocument;
+  }
+
+  @Given("the following transmissions stored")
+  public void given_an_transmission_stored_petitions(List<TransmissionDocument> transmissionDocumentList) {
+    transmissionRepository.saveAll(transmissionDocumentList);
   }
 
   @When("recover all transmissions")
@@ -48,10 +104,14 @@ public class GetTransmissions extends CucumberSpringConfiguration {
         .exchange();
   }
 
-  @Then("returns a list with all transmissions")
-  public void returns_a_list_with_all_transmissions() {
+  @Then("returns a list with {numeric} transmission(s)")
+  public void returns_a_list_with_all_transmissions(Integer nDocuments) {
     responseSpec.expectBodyList(TransmissionResponse.class)
-        .hasSize(1)
-        .contains(TransmissionResponseBuilder.build());
+        .hasSize(nDocuments);
+  }
+
+  @Then("the response was ok")
+  public void the_response_was_ok() {
+    responseSpec.expectStatus().isOk();
   }
 }
--- bdd-test/src/test/resources/cucumber/features/GetTransmissions.feature
+++ bdd-test/src/test/resources/cucumber/features/GetTransmissions.feature
@@ -5,8 +5,35 @@ Feature: Get a list of all active transmission stored
   I want to recover all transmissions
   In order to know all spaceship current transmissions
 
-  Scenario: Get all active transmission stored
+  Background:
+    Given no transmission stored
+
+  Scenario: Get an active transmission stored
     Given an active transmission stored with petitions
+    And tree inactive transmissions stored with petitions
     When recover all transmissions
-    Then returns a list with all transmissions
+    Then returns a list with a transmission
+    And the response was ok
+
+  Scenario: Get two active transmission stored
+    Given two active transmissions stored with petitions
+    And one inactive transmission stored with petitions
+    And tree inactive transmissions stored without petitions
+    When recover all transmissions
+    Then returns a list with two transmissions
+    And the response was ok
+
+  Scenario: Get only active transmissions stored
+    Given the following transmissions stored
+      | id | active | hasPetitions |
+      | 10  | true   | false        |
+      | 20  | false  | true         |
+      | 30  | true   | false        |
+      | 40  | true   | true         |
+
+    When recover all transmissions
+    Then returns a list with tree transmissions
+    And the response was ok
+
+
     
