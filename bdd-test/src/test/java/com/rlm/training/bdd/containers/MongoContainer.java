c1bd10d1824ff0c464aa962ab8f6ed2f519a0050
rlmelgar <rlmelgar@gmail.com>
2023-11-30 11:07:51 +0100

    [RLM-TR-BDD-11-BDD] feat: BDD and testcontainers

--- bdd-test/src/test/java/com/rlm/training/bdd/containers/MongoContainer.java
+++ bdd-test/src/test/java/com/rlm/training/bdd/containers/MongoContainer.java
@@ -1,4 +1,4 @@
-package cucumber;
+package com.rlm.training.bdd.containers;
 
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.testcontainers.containers.MongoDBContainer;
 
 @TestConfiguration(proxyBeanMethods = false)
-public class ContainersConfig {
+public class MongoContainer {
 
   @Bean
   @ServiceConnection
